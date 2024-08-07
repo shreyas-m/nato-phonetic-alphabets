85c04afed1122c8b5d8e8a4c0224f48498fb865a
Shreyas Mahajan <maharaj.shreyu@gmail.com>
2024-08-07 18:42:20 +0100

    Nato help

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas
 
 nato_alpha = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-nato_dict = {row.letter:row.code for (index, row) in nato_alpha.iterrows()}
+nato_dict = {row.letter: row.code for (index, row) in nato_alpha.iterrows()}
 print(nato_dict)
 
 name = input("Enter your name : ").upper()
